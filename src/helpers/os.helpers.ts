2be6d8befb558bdf2f07a4bc2a176fb2e19aa9c3
Mark <mwoyshvillo@gmail.com>
2023-03-31 12:10:40 +0300

    add pop up with browser support

--- src/helpers/os.helpers.ts
+++ src/helpers/os.helpers.ts
@@ -13,3 +13,28 @@ export function isAndroid() {
 export function isMobile() {
   return isAndroid() || isIos()
 }
+
+export function isChromeBrowser() {
+  const res = detect()
+  return res && res.name === 'chrome'
+}
+
+export function isIosBrowser() {
+  const res = detect()
+  return res && (res.name === 'ios-webview' || res.name === 'ios')
+}
+
+export function isOperaBrowser() {
+  const res = detect()
+  return res && res.name === 'opera'
+}
+
+export function isEdgeBrowser() {
+  const res = detect()
+  return res && res.name === 'edge'
+}
+
+export function isFirefoxBrowser() {
+  const res = detect()
+  return res && res.name === 'firefox'
+}
