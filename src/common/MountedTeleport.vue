51e7dbdd2f54051153f710e665c14026bf83988c
Mark <mwoyshvillo@gmail.com>
2023-06-21 15:43:49 +0300

    add mounted teleport

--- src/common/MountedTeleport.vue
+++ src/common/MountedTeleport.vue
@@ -5,13 +5,39 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import { sleep, ErrorHandler } from '@/helpers'
 
-defineProps<{ to: string }>()
+const MAX_RETRIES = 10
+const DEFAULT_RETRY_INTERVAL = 100
+
+const props = defineProps<{ to: string }>()
 
 const isMounted = ref(false)
 
-onMounted(() => {
-  isMounted.value = true
+const waitUntilTargetMounted = async (retryDelay = DEFAULT_RETRY_INTERVAL) => {
+  let teleportTarget = document.querySelector(props.to)
+  let tryNumber = 0
+
+  while (!teleportTarget) {
+    if (tryNumber > MAX_RETRIES) throw new Error('No node with such id found')
+
+    teleportTarget = document.querySelector(props.to)
+
+    if (teleportTarget) break
+
+    tryNumber++
+    await sleep(retryDelay)
+  }
+}
+
+onMounted(async () => {
+  try {
+    await waitUntilTargetMounted()
+
+    isMounted.value = true
+  } catch (error) {
+    ErrorHandler.process(error)
+  }
 })
 </script>
